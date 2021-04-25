1370389e43d25fc95321cb99a1c1b88f9963e433
txr <txr@protonmail.com>
2021-04-25 22:03:37 +0100

    first version

---
+++ sista-node-example/db.js
@@ -0,0 +1,30 @@
+const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://localhost", { useUnifiedTopology: true })
+            .then(conn => global.conn = conn.db("sistca-node-db"))
+            .catch(err => console.log(err))
+
+function findAll() {
+    return global.conn.collection("sensorslog").find().toArray();
+}
+
+function insert(sensor_record) {
+    return global.conn.collection("sensorslog").insertOne(sensor_record);
+}
+
+const ObjectId = require("mongodb").ObjectId;
+function findOne(id) {
+    return global.conn.collection("sensorslog").findOne(new ObjectId(id));
+}
+
+function update(id, sensor_record) {
+    //const editedValues = {}
+    console.log('update:: id: ' + id + ' record: ');
+    console.log(sensor_record);
+    return global.conn.collection("sensorslog").updateOne({ _id: new ObjectId(id) }, { $set: sensor_record });
+}
+
+function deleteOne(id) {
+    return global.conn.collection("sensorslog").deleteOne({ _id: new ObjectId(id) });
+}
+ 
+module.exports = { findAll, insert, findOne, update, deleteOne }
---
+++ sista-node-example/routes/index.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+  try {
+    const docs = await global.db.findAll();
+    res.render('index', { title: 'Sensors log list', docs });
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.get('/add', (req, res, next) => {
+  const location = {"lat": "", "lng": ""};
+  res.render('add', { title: 'Add new sensor record', doc: {"sensor_id": "", "type": "", "value": "", "location": location, "unix_timestamp": ""}, action: '/add'});
+});
+
+router.post('/add', async (req, res, next) => {
+  // Acquire values from HTTP command
+  const sensor_id = parseInt(req.body.sensor_id);
+  const type = req.body.type;
+  const value = parseFloat(req.body.value);
+  const latitude = parseFloat(req.body.latitude);
+  const longitude = parseFloat(req.body.longitude);
+  const timestamp = Date.now();
+  
+  // Insert data into the database
+  try {
+    const result = await global.db.insert({ "sensor_id": sensor_id, "type": type, "value": value, "location": {"lat": latitude, "lng": longitude}, "unix_timestamp": timestamp});
+    console.log(result);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/edit/:id', async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const doc = await global.db.findOne(id);
+    res.render('add', { title: 'Edit record', doc, action: '/edit/' + doc._id });
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.post('/edit/:id', async (req, res, next) => {
+  // Acquire values from HTTP command
+  const id = req.params.id;
+  const sensor_id = parseInt(req.body.sensor_id);
+  const type = req.body.type;
+  const value = parseFloat(req.body.value);
+  const latitude = parseFloat(req.body.latitude);
+  const longitude = parseFloat(req.body.longitude);
+  const timestamp = Date.now();
+  
+  const upd_data = { "sensor_id": sensor_id, "type": type, "value": value, "location": {"lat": latitude, "lng": longitude}, "unix_timestamp": timestamp};
+
+  try {
+    const result = await global.db.update(id, upd_data);
+    console.log(result);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.get('/del/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await global.db.deleteOne(id);
+    console.log(result);
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.get('/export', async (req, res, next) => {
+  console.log("EXPORT");
+  const fs = require('fs');
+
+  const docs = await global.db.findAll();
+  const data = JSON.stringify(docs);
+  res.setHeader('Content-disposition', 'attachment; filename= myFile.json');
+  res.setHeader('Content-type', 'application/json');
+  res.write(data, function (err) {
+  res.end();
+  });
+});
+
+module.exports = router;
